d5c669dde7e0f5a61d9945a25009b156cf4ac5ea
philippe44 <philippe44@users.noreply.github.com>
2026-08-05 22:48:55 +0200

    0.20.4

--- spotupnp/src/spotupnp.h
+++ spotupnp/src/spotupnp.h
@@ -22,7 +22,7 @@
 #include "metadata.h"
 #include "spotify.h"
 
-#define VERSION "v0.20.3"" ("__DATE__" @ "__TIME__")"
+#define VERSION "v0.20.4"" ("__DATE__" @ "__TIME__")"
 
 /*----------------------------------------------------------------------------*/
 /* typedefs */
